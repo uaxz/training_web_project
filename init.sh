46268669fe2ecbebea5431667e57d72f6ba1c0c2
uaxz <45533130+uaxz@users.noreply.github.com>
2019-09-17 22:25:47 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#mkdir uploads etc public public/img public/css public/js
+mkdir uploads etc public public/img public/css public/js
