0547850c8fb99f568433ceb4552a5cb573824511
abstractor <uaxzzzz@gmail.com>
2019-09-17 16:51:18 +0300

    4

--- init.sh
+++ init.sh
@@ -1,1 +1,4 @@
-mkdir uploads etc public public/img public/css public/js
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+#mkdir uploads etc public public/img public/css public/js
