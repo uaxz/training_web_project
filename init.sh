864341f5b1e3964b0af72daec2244576aae34742
abstractor <uaxzzzz@gmail.com>
2019-09-17 22:37:42 +0300

    5

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#mkdir uploads etc public public/img public/css public/js
+mkdir uploads etc public public/img public/css public/js
