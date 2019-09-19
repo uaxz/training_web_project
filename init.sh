6d0c39581b97ea9d9c6de243c1fe62b69ccbcf97
abstractor <uaxzzzz@gmail.com>
2019-09-19 23:41:47 +0300

    add django

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+sudo pip3 install --upgrade django
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
