7010c1dcd8bbbd9ab1f0abb0ded8cbc695d155d1
abstractor <uaxzzzz@gmail.com>
2019-09-20 00:21:33 +0300

    add django 3

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
-sudo pip3 install --upgrade django
-sudo pip3 install --upgrade gunicorn
+sudo pip3 install --upgrade django=2.0.0
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf --bind=0.0.0.0:8080 hello:application
 sudo /etc/init.d/nginx restart
-#mkdir uploads etc public public/img public/css public/js
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf --bind=0.0.0.0:8080 hello:application &
